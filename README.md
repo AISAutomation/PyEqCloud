52bc5dd40a40ad04347e55d92a71f143d96cf99f
Thiem <patrick.thiem@ais-automation.com>
2020-05-06 11:09:34 +0200

    ready for building package

--- README.md
+++
@@ -1,2 +0,0 @@
-# PyEqCloud
-Python module to connect with python to the Kontron AIS EquipmentCloud
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyEqCloud',
+    version='0.1.0',
+    description='Gather Data via a REST-Connection from the Kontron AIS GmbH EquipmentCloud',
+    author='Patrick Thiem',
+    author_email='Patrick.Thiem@kontron-ais.com'
+    py_modules=["EqCloudRestApiWrapper"],
+    package_dir={'': 'src'},
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Data Scientists, Developers',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    keywords='equipment cloud',
+    packages=find_packages(exclude=['docs','tests*']),
+    install_requires=['requests','pandas','getpass','tqdm'],
+    data_files=None
+)
